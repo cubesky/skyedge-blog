0450737195dd05d21bd496c6e85c74c22b4f8abd
cubesky <wqr3011@gmail.com>
2017-09-08 11:41:24 +0000

    Site updated: 2017-09-08 11:41:23

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const version = 'v201709081038';
+const version = 'v201709081141';
 const __DEVELOPMENT__ = false;
 const __DEBUG__ = false;
 const offlineResources = ['/', '/offline.html', '/offline.svg', '/Lucca-Regular.otf'];
