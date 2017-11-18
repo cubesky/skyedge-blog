ca170b0374ef2b5ade922e228cc4ea2e604b9fa6
cubesky <wqr3011@gmail.com>
2017-11-18 06:30:31 +0000

    Site updated: 2017-11-18 06:30:30

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 "use strict";
 (function() {
-    var cacheVersion = "201711131410";
+    var cacheVersion = "201711180630";
     var staticImageCacheName = "image" + cacheVersion;
     var staticAssetsCacheName = "assets" + cacheVersion;
     var contentCacheName = "content" + cacheVersion;
