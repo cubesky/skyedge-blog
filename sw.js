9c331a96060b9c81d64080fae6ef771fd82ab2b9
cubesky <wqr3011@gmail.com>
2018-01-02 15:00:08 +0000

    Site updated: 2018-01-02 15:00:07

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 "use strict";
 (function() {
-    var cacheVersion = "201801021457";
+    var cacheVersion = "201801021500";
     var staticImageCacheName = "image" + cacheVersion;
     var staticAssetsCacheName = "assets" + cacheVersion;
     var contentCacheName = "content" + cacheVersion;
